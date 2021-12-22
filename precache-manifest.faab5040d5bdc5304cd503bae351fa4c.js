1618bdf6550e09d96724c44eb953d895e5461689
komkawila <kompanuwat@gmail.com>
2021-12-22 08:53:53 +0700

    first commit

---
+++ precache-manifest.faab5040d5bdc5304cd503bae351fa4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f1263265486c6e3a4b55796b0851e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77a49a8833adaa4d01e0",
+    "url": "/static/css/main.e05e02fd.chunk.css"
+  },
+  {
+    "revision": "f264e311cbf73985e7ac",
+    "url": "/static/js/2.9cbf0011.chunk.js"
+  },
+  {
+    "revision": "f89ec0e947fe52779183e6ebdf1dc0ff",
+    "url": "/static/js/2.9cbf0011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b426221802ddd7a5d5b7",
+    "url": "/static/js/3.dc090615.chunk.js"
+  },
+  {
+    "revision": "77a49a8833adaa4d01e0",
+    "url": "/static/js/main.afd5edc4.chunk.js"
+  },
+  {
+    "revision": "a700ae2b8f0b63c44032",
+    "url": "/static/js/runtime-main.856744ef.js"
+  }
+]);
